292bc8329588990e853d02d4b96c1ce784fd4fb8
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-09-03 20:03:00 +0500

    fix scroll and add page2

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -17,6 +17,10 @@ if(document.querySelector(article)) {
             el: '.swiper-pagination',
         },
         breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
             1200: {
                 slidesPerView: 3,
                 spaceBetween: 40,
@@ -25,6 +29,39 @@ if(document.querySelector(article)) {
         navigation: {
             nextEl: '.article__button-next-js',
             prevEl: '.article__button-prev-js',
+        },
+    })
+}
+
+
+const article1 = '.article1-js ';
+
+if(document.querySelector(article1)) {
+    const article1Slider = new Swiper(article1, {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        centeredSlidesBounds: true,
+        effect: "fade",
+        loop: true,
+        fadeEffect: {
+          crossFade: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1300: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            }
+        },
+        navigation: {
+            nextEl: '.article1__button-next-js',
+            prevEl: '.article1__button-prev-js',
         },
     })
 }
--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -2,6 +2,8 @@ import Swiper, { Navigation } from "swiper";
 Swiper.use([Navigation]);
 
 const feedback = '.feedback-js';
+const feedback1 = '.feedback1-js';
+
 
 if(document.querySelector(feedback)) {
     const feedbackSlider = new Swiper(feedback, {
@@ -17,6 +19,33 @@ if(document.querySelector(feedback)) {
         breakpoints: {
             992: {
                 spaceBetween: 40,  
+            },
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.feedback__button-next-js',
+            prevEl: '.feedback__button-prev-js',
+        },
+    })
+}
+
+if(document.querySelector(feedback1)) {
+    const feedback1Slider = new Swiper(feedback1, {
+        slidesPerView: 'auto',
+        spaceBetween: 15,
+        effect: "fade",
+        loop: true,
+        // centeredSlides: true,
+        centeredSlidesBounds: true,
+        fadeEffect: {
+          crossFade: true
+        },
+        breakpoints: {
+            1200: {
+                spaceBetween: 35,
+                slidesPerView: 5,
             },
         },
         pagination: {
